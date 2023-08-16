c8ef2a8f7882356fa8a4223515c2fe675b22bb6b
jodosjodos <jeandedieu2030@gmail.com>
2023-08-17 01:46:17 +0200

    adding  history

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "color":"#b7ab98",
+        "mouse":"#eb5939"
+
+      }
+    },
   },
   plugins: [],
 }
-
